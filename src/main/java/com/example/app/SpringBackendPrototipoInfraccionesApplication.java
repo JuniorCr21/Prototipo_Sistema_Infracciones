5a1720ab0ca304d08947be977c1b50c8db429938
carri <carri@DESKTOP-FCN93BH>
2020-08-18 23:25:38 -0500

    Iniciando

---
+++ src/main/java/com/example/app/SpringBackendPrototipoInfraccionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendPrototipoInfraccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendPrototipoInfraccionesApplication.class, args);
+	}
+
+}
